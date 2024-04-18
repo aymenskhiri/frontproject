18b3e28564185cf8e355a22cda165f3c3c543236
aymenskhiri <skhirimohamedaymen@gmail.com>
2024-04-18 03:40:10 +0100

    SignUp

--- src/Components/LoginSignup/Login.jsx
+++ src/Components/LoginSignup/Login.jsx
@@ -1,34 +1,78 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios';
 import './LoginSignup.css'
 import { Link } from 'react-router-dom';
 const Login = () => {
-  return (
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+
+    // test a get request
+    const testGetRequest = () => {
+        fetch('https://localhost:7247/WeatherForecast', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
+            .then(data => {
+                console.log(data);
+            })
+            .catch(error => {
+                console.error('There was a problem with the fetch operation:', error);
+            });
+    };
+    // post request to login
+    const submitLogin = async (event) => {
+        event.preventDefault(); 
+        try {
+            const response = await axios.post('https://localhost:7247/api/User/login', {
+                email: email,
+                password: password
+            });
+    
+            console.log(response.data); 
+        } catch (error) {
+            console.error('Error occurred:', error.response.data); 
+        }
+    };
+    return (
 
         <div className='container'>
-        <form action="">
-        <div className='header'>
-        <div className='text'>Login</div>
-        <div className='underline'></div>
+            <form onSubmit={submitLogin}>
+                <div className='header'>
+                    <div className='text'>Login</div>
+                    <div className='underline'></div>
 
 
+                </div>
+                <div className="inputs">
+                    <div className="input">
+                        <input type='email' placeholder='Email' value={email}
+                            onChange={(e) => setEmail(e.target.value)} />
+                    </div>
+                    <div className="input">
+                        <input type='password' placeholder='Mot de passe' value={password}
+                            onChange={(e) => setPassword(e.target.value)} />
+                    </div>
+                </div>
+                {error && <div className='error'>{error}</div>}
+                <div className="submit-container">
+                    <button type='submit'>Login</button>
+                </div>
+                <div className="register-link">
+                    <p>Vous n'avez pas de compte ? <Link to="/signup">Inscription</Link></p>
+                </div>
+            </form>
+            <button onClick={testGetRequest} >Test Get Request</button>
         </div>
-        <div className="inputs">
-        <div className="input">
-            <input type='email' placeholder='Email' />
-        </div>
-        <div className="input">
-            <input type='password' placeholder='Mot de passe' />
-        </div>
-        </div>
-        <div className="submit-container">
-            <button type='submit'>Login</button>
-        </div>
-        <div className="register-link">
-        <p>Vous n'avez pas de compte ? <Link to="/signup">Inscription</Link></p>
-        </div>
-        </form>
-    </div>
-  )
+    )
 }
 
 export default Login
--- src/Components/LoginSignup/Signup.jsx
+++ src/Components/LoginSignup/Signup.jsx
@@ -1,12 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './LoginSignup.css'
+import axios from 'axios';
 
 
 
 const Signup = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [nom, setnom] = useState('');
+    const [prenom, setprenom] = useState('');
+    const [adresse, setAdresse] = useState('');
+    const [tel, setTel] = useState('');
+    const [error, setError] = useState('');
+
+    // post request to signup
+    const submitSignup = async (event) => {
+        event.preventDefault(); 
+        try {
+            const response = await axios.post('https://localhost:7247/api/User/add-user', {
+                email: email,
+                password: password,
+                nom: nom,
+                prenom: prenom,
+                adresse: adresse,
+                tel: tel
+            });
+    
+            console.log(response.data); 
+        } catch (error) {
+            console.error('Error occurred:', error.response.data); 
+        }
+    };
+
   return (
     <div className='container'>
-           <form action="">
+           <form onSubmit={submitSignup}>
         <div className='header'>
         <div className='text'>Sign Up</div>
         <div className='underline'></div>
@@ -15,24 +43,25 @@ const Signup = () => {
         </div>
         <div className="inputs">
         <div className="input">
-            <input type='email' placeholder='Email' />
+            <input type='email' placeholder='Email' value={email} onChange={(e) => setEmail(e.target.value)} />
         </div>
         <div className="input">
-            <input type='password' placeholder='Mot de passe' />
+            <input type='password' placeholder='Mot de passe' value={password} onChange={(e) => setPassword(e.target.value)}/>
         </div>
         <div className="input">
-            <input type='text' placeholder='Nom' />
+            <input type='text' placeholder='Nom' value={nom} onChange={(e) => setnom(e.target.value)}/>
         </div>
         <div className="input">
-            <input type='text' placeholder='Prénom' />
+            <input type='text' placeholder='Prénom'  value={prenom} onChange={(e) => setprenom(e.target.value)}/>
         </div>
         <div className="input">
-            <input type='text' placeholder='Adresse' />
+            <input type='text' placeholder='Adresse' value={adresse} onChange={(e) => setAdresse(e.target.value)} />
         </div>
         <div className="input">
-            <input type='int' placeholder='Téléphone' />
+            <input type='int' placeholder='Téléphone' value={tel} onChange={(e) => setTel(e.target.value)}/>
         </div>
         </div>
+        {error && <div className='error'>{error}</div>}
         <div className="submit-container">
             <button type='submit'>Sign Up</button>
         </div>
